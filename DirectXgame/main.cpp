e5315d0b3f1fb8e3fec39cfa741e93ee343a8039
Takanaga Daiki <k024g0112@g.neec.ac.jp>
2025-04-18 14:20:56 +0900

    01_03

--- DirectXgame/main.cpp
+++ DirectXgame/main.cpp
@@ -11,6 +11,9 @@
 #include<DbgHelp.h>
 #pragma comment(lib, "Dbghelp.lib")
 
+#include<dxgidebug.h>
+#pragma comment(lib, "dxguid.lib")
+
 #pragma comment(lib, "d3d12.lib")
 #pragma comment(lib, "dxgi.lib")
 
@@ -461,6 +464,32 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	}
 	//出力ウィンドウへの文字出力
 	OutputDebugStringA("Hello, DirectX!\n");
+	IDXGIDebug1* debug;
+	if (SUCCEEDED(DXGIGetDebugInterface1(0, IID_PPV_ARGS(&debug)))) {
+		debug->ReportLiveObjects(DXGI_DEBUG_ALL,DXGI_DEBUG_RLO_ALL);
+		debug->ReportLiveObjects(DXGI_DEBUG_APP, DXGI_DEBUG_RLO_ALL);
+		debug->ReportLiveObjects(DXGI_DEBUG_D3D12, DXGI_DEBUG_RLO_ALL);
+		debug->Release();
+	}
+
+	//解放処理
+	CloseHandle(fenceEvent);
+	fence->Release();
+	rtvDescriptorHeap->Release();
+	swapChainResources[0]->Release();
+	swapChainResources[1]->Release();
+	swapChain->Release();
+	commandList->Release();
+	commandAllocator->Release();
+	commandQueue->Release();
+	device->Release();
+	useAdapter->Release();
+	dxgiFactory->Release();
+#ifdef _DEBUG
+	debugController->Release();
+#endif // _DEBUG
+
+	CloseWindow(hwnd);
 
 	return 0;
 }
