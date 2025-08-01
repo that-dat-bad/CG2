8c570a097b368be9cdb8c21039126ad026cd071b
Takanaga Daiki <k024g0112@g.neec.ac.jp>
2025-08-01 22:14:55 +0900

    完了 乙

--- DirectXgame/Object3d.PS.hlsl
+++ DirectXgame/Object3d.PS.hlsl
@@ -3,24 +3,24 @@
 
 struct PixelShaderOutput
 {
-    float4 color : SV_TARGET;
+    float32_t4 color : SV_TARGET;
 };
 
 // マテリアル情報を格納する定数バッファ
 struct Material
 {
-    float4 color;
+    float32_t4 color;
     int enableLighting;
     float shininess;
-    float2 padding;
+    float32_t2 padding;
     matrix uvTransform;
 };
 
 // 平行光源の情報を格納する定数バッファ
 struct DirectionalLight
 {
-    float4 color;
-    float3 direction;
+    float32_t4 color;
+    float32_t3 direction;
     float intensity;
 };
 
@@ -28,35 +28,34 @@ struct DirectionalLight
 struct LightingSettings
 {
     int lightingModel; // 0: Lambert, 1: Half-Lambert
-    float3 padding;
+    float32_t3 padding;
 };
 
 ConstantBuffer<Material> gMaterial : register(b0);
 ConstantBuffer<DirectionalLight> gDirectionalLight : register(b1);
 ConstantBuffer<LightingSettings> gLightingSettings : register(b2);
 
-Texture2D<float4> gTexture : register(t0);
+Texture2D<float32_t4> gTexture : register(t0);
 SamplerState gSampler : register(s0);
 
 // 頂点シェーダーからの入力
 struct PixelInput
 {
-    float4 position : SV_POSITION;
-    float2 texcoord : TEXCOORD0;
-    float3 normal : NORMAL0;
-    float3 worldPosition : WORLDPOSITION0;
+    float32_t4 position : SV_POSITION;
+    float32_t2 texcoord : TEXCOORD0;
+    float32_t3 normal : NORMAL0;
+    float32_t3 worldPosition : WORLDPOSITION0;
 };
 
 PixelShaderOutput main(PixelInput input)
 {
     PixelShaderOutput output;
 
-    float4 texColor = float4(1.0, 1.0, 1.0, 1.0); // UV無い時は白
-
-    // UVがあるときだけサンプル（例：shininess >= 0 が目印）
+    float32_t4 texColor = float32_t4(1.0, 1.0, 1.0, 1.0); // UV無い時は白
+    
     if (gMaterial.shininess >= 0.0f)
     {
-        float2 transformedUV = mul(float4(input.texcoord, 0.0, 1.0), gMaterial.uvTransform).xy;
+        float32_t2 transformedUV = mul(float32_t4(input.texcoord, 0.0, 1.0), gMaterial.uvTransform).xy;
         texColor = gTexture.Sample(gSampler, transformedUV);
     }
 
@@ -66,8 +65,8 @@ PixelShaderOutput main(PixelInput input)
     // ライティングが有効な場合
     if (gMaterial.enableLighting != 0)
     {
-        float3 N = normalize(input.normal);
-        float3 L = normalize(gDirectionalLight.direction);
+        float32_t3 N = normalize(input.normal);
+        float32_t3 L = normalize(gDirectionalLight.direction);
         float NdotL = dot(N, L);
 
         float diffuse = 0.0f;
@@ -81,10 +80,11 @@ PixelShaderOutput main(PixelInput input)
             diffuse = diffuse * diffuse;
         }
 
-        float3 light = gDirectionalLight.color.rgb * gDirectionalLight.intensity * diffuse;
+        float32_t3 light = gDirectionalLight.color.rgb * gDirectionalLight.intensity * diffuse;
 
         // 光を反映
         output.color.rgb *= light;
+        
     }
 
     return output;
